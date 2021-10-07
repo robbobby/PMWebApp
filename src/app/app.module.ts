473bc9760b1abae99235ac8f245001c8c6fab615
robbobby <69080884+robbobby@users.noreply.github.com>
2021-10-07 15:06:06 +0100

    Added new component and registered the modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
+import {HomeComponent} from "./Home";
+import {LoginComponent} from "./Login";
+import {RegisterComponent} from "./Register";
 
 @NgModule( {
     imports: [BrowserModule],
-    declarations: [AppComponent],
+    declarations: [AppComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent
+    ],
     bootstrap: [AppComponent]
 })
 
