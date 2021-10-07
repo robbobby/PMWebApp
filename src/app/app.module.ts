dac2786901938f46e03a53481be3313caf760371
robbobby <69080884+robbobby@users.noreply.github.com>
2021-10-07 17:25:30 +0100

    Added a authentication service to work with the fake backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
-import {HomeComponent} from "./Home";
-import {LoginComponent} from "./Login";
-import {RegisterComponent} from "./Register";
-import {appRoutingModule} from "./app.routing";
+import { HomeComponent } from "./Home";
+import { LoginComponent } from "./Login";
+import { RegisterComponent } from "./Register";
+import { appRoutingModule } from "./app.routing";
+import { FakeBackEndInterceptor } from "./helpers";
+import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule( {
     imports: [
         BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
         appRoutingModule
     ],
 
@@ -21,7 +26,10 @@ import {appRoutingModule} from "./app.routing";
     ],
 
     bootstrap: [
-        AppComponent
+        AppComponent,
+    ],
+    providers: [
+        FakeBackEndInterceptor
     ]
 })
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -18,7 +19,12 @@ module.exports = {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: './src/index.html' })
+        new HtmlWebpackPlugin({ template: './src/index.html' }),
+        new webpack.DefinePlugin({
+            config: JSON.stringify({
+                apiUrl: 'http://localhost:4000'
+            })
+        })
     ],
     devServer: {
         historyApiFallback: true
